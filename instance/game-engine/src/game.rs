4c0e07b7471ff60a93c707db95d869e41f139e2a
AppleHair <eytanzury@gmail.com>
2023-08-05 18:37:32 +0300

    Added 'Scene' map

--- instance/game-engine/src/game.rs
+++ instance/game-engine/src/game.rs
@@ -2,13 +2,15 @@
 use crate::webapp;
 
 use std::collections::HashMap;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 use rhai::{Engine, Scope, AST, Map, packages::Package, packages::StandardPackage, EvalAltResult, Dynamic};
 
 mod scene;
 
 //
-#[derive(Clone, PartialEq)]
+#[derive(Clone)]
 pub enum EntityRow {
     Manager,
     Scene(u32),
@@ -62,18 +64,16 @@ impl EntityDefinition {
     }
 }
 
-
-
 //
-struct EntityInstance<'a> {
-    definition: &'a EntityDefinition,
+struct EntityInstance {
+    definition: Rc<EntityDefinition>,
     scope: Scope<'static>,
 }
 
 //
-impl<'a> EntityInstance<'a> {
+impl EntityInstance {
     //
-    fn new(engine: &Engine, def: &'a EntityDefinition) -> Result<Self, (EntityRow, Box<EvalAltResult>)> {
+    fn new(engine: &Engine, def: Rc<EntityDefinition>) -> Result<Self, (EntityRow, Box<EvalAltResult>)> {
         //
         let mut inst = Self {
             //
@@ -82,11 +82,21 @@ impl<'a> EntityInstance<'a> {
             scope: Scope::new(),
         };
         //
-        let err = engine.run_ast_with_scope(&mut inst.scope, &def.script).err();
+        match inst.definition.reference {
+            //
+            EntityRow::Scene(_) => {
+                //
+                inst.scope.push("Scene", scene::create_scene(&engine, &inst.definition.config));
+            },
+            //
+            _ => {},
+        }
+        //
+        let err = engine.run_ast_with_scope(&mut inst.scope, &inst.definition.script).err();
         //
         if err.is_some() {
             //
-            return Err((def.reference.to_owned(), err.unwrap()));
+            return Err((inst.definition.reference.to_owned(), err.unwrap()));
         }
         //
         Ok(inst)
@@ -108,14 +118,38 @@ impl<'a> EntityInstance<'a> {
 }
 
 //
-fn create_api(entity_defs: &mut HashMap<u32,EntityDefinition>) -> Result<(Engine, EntityInstance), (EntityRow, Box<EvalAltResult>)> {
+fn create_api(entity_defs: &mut HashMap<u32,Rc<EntityDefinition>>) -> Result<(Engine, EntityInstance, Rc<RefCell<EntityInstance>>, u32), (EntityRow, Box<EvalAltResult>)> {
     // Create an 'Engine'
     let mut engine = Engine::new_raw();
 
     // Register API features to the 'Engine'
-    engine.on_print(|text| {
-        webapp::log(text);
-    });
+    engine.on_print(|text| { webapp::log(text); })
+          .register_type_with_name::<scene::Camera>("Camera")
+          .register_get_set("x", scene::Camera::get_x, scene::Camera::set_x)
+          .register_set("x", scene::Camera::set_x_rhai_int)
+          .register_set("x", scene::Camera::set_x_rhai_float)
+          .register_get_set("y", scene::Camera::get_y, scene::Camera::set_y)
+          .register_set("y", scene::Camera::set_y_rhai_int)
+          .register_set("y", scene::Camera::set_y_rhai_float)
+          .register_get_set("zoom", scene::Camera::get_zoom, scene::Camera::set_zoom)
+          .register_set("zoom", scene::Camera::set_zoom_rhai_int)
+          .register_set("zoom", scene::Camera::set_zoom_rhai_float)
+          .register_fn("to_string", scene::Camera::to_string)
+          .register_type_with_name::<scene::Layer>("Layer")
+          .register_get("name", scene::Layer::get_name)
+          .register_fn("to_string", scene::Layer::to_string)
+          .register_type_with_name::<scene::Scene>("Scene")
+          .register_get_set("width", scene::Scene::get_width, scene::Scene::set_width)
+          .register_set("width", scene::Scene::set_width_rhai_int)
+          .register_set("width", scene::Scene::set_width_rhai_float)
+          .register_get_set("height", scene::Scene::get_height, scene::Scene::set_height)
+          .register_set("height", scene::Scene::set_height_rhai_int)
+          .register_set("height", scene::Scene::set_height_rhai_float)
+          .register_get_set("in_color", scene::Scene::get_inside_color, scene::Scene::set_inside_color)
+          .register_get_set("out_color", scene::Scene::get_outside_color, scene::Scene::set_outside_color)
+          .register_get_set("camera", scene::Scene::get_camera, scene::Scene::set_camera)
+          .register_get("layers", scene::Scene::get_layers)
+          .register_fn("to_string", scene::Scene::to_string);
 
     // Register the Standard Package
     let package = StandardPackage::new();
@@ -125,12 +159,22 @@ fn create_api(entity_defs: &mut HashMap<u32,EntityDefinition>) -> Result<(Engine
     // Insert a new entity definition into the hash map.
     // This definition will define the state manager, and
     // will store its script and config data.
-    entity_defs.insert(0, EntityDefinition::new(&engine, EntityRow::Manager)?);
+    entity_defs.insert(0, 
+        Rc::new(
+            EntityDefinition::new(&engine, 
+                EntityRow::Manager
+            )?
+        )
+    );
     // Create a new entity instance for the state manager.
     // This instance will borrow its definition and contain
     // the entity's 'Scope'.
-    let mut state_manager = EntityInstance::new(&engine, entity_defs.get(&0)
-        .expect("entity_defs.get(&0) should have had the state manager's definition by now"))?;
+    let mut state_manager: EntityInstance = EntityInstance::new(&engine, 
+        Rc::clone(
+            entity_defs.get(&0)
+            .expect("entity_defs.get(&0) should have had the state manager's definition by now")
+        )
+    )?;
 
     // Take the 'State' object map from 
     // the state manager and turn it into
@@ -138,68 +182,116 @@ fn create_api(entity_defs: &mut HashMap<u32,EntityDefinition>) -> Result<(Engine
     let state_map = state_manager.scope.remove::<Dynamic>("State").unwrap_or(Dynamic::UNIT).into_shared();
     // If it's not a ()
     if !state_map.is_unit() {
-        // Push it back into the state manager as
+        // Push it back into the state manager
         // as a shared object map variable.
         state_manager.scope.push("State", state_map.clone());
         // Register a variable resolver.
         engine.on_var(move |name, _, context| {
             match name {
-                // Continue with the normal variable resolution process
-                // if the "State" variable is defined inside the context's
-                // scope (which should be the state manager's), but if "State"
-                // isn't defined, return a flat copy of the shared state map.
-                "State" => if context.scope().contains(name) { Ok(None) } else { Ok(Some(state_map.flatten_clone())) },
-                // Continue with the normal variable resolution process.
+                // If the name of the
+                // accessed variable is 'State'
+                "State" => {
+                    if context.scope().contains(name) {
+                        // If the variable exists
+                        // in the scope already
+                        // (which means it's the
+                        // state manager's scope)
+                        Ok(None) 
+                    } else {
+                        // Otherwise, return a clone
+                        // of the value of the state map
+                        Ok(Some(state_map.flatten_clone())) 
+                    }
+                },
+                // Otherwise, continue with the normal variable resolution process.
                 _ => Ok(None)
             }
         });
         // Register a variable definition filter.
         engine.on_def_var(|_, info, context| {
             match info.name {
-                // Continue with the normal variable definition process
-                // if the "State" variable is already defined inside the 
-                // context's global scope (which should be the state manager's),
-                // but if "State" isn't defined or defined not in the global 
-                // scope, Prevent the variable definition process.
-                "State" => if !context.scope().contains(info.name) { Err("Can't define State outside the State Manager script".into()) } 
-                           else if && info.nesting_level > &&(0 as usize)  { Err("Can't define State outside the global scope".into()) } 
-                           else { Ok(true) },
-                // Continue with the normal variable definition process.
-                _ => Ok(true)
+                // If the name of the
+                // defined variable is 'State'
+                "State" => {
+                    if !context.scope().contains(info.name) {
+                        // If the variable doesn't
+                        // exist in the scope already
+                        // (which means it's not the 
+                        // state manager's scope)
+                        return Err("Can't define State outside the State Manager script".into());
+                    } else if && info.nesting_level > &&(0 as usize)  {
+                        // If the variable is being
+                        // defined outside the global scope
+                        return Err("Can't define State outside the global scope".into());
+                    } else {
+                        return Ok(true);
+                    } 
+                },
+                // Otherwise, continue with the normal variable definition process,
+                // where script runtime definitions of 'Scene' or 'Object' are forbidden.
+                _ => Ok((info.name != "Scene" && info.name != "Object") || !info.will_shadow)
             }
         });
     }
 
-    // The API is Done!
-    Ok((engine, state_manager))
+    // Receive the rowid of the initial scene from the the state manager's config
+    let mut cur_scene_id = state_manager.definition.config["initial-scene"].as_int()
+        .expect("The value of 'initial-scene' in the state manager's config should be an integer") as u32;
+    //
+    entity_defs.insert(cur_scene_id, 
+        Rc::new(
+            EntityDefinition::new(&engine, 
+                EntityRow::Scene(cur_scene_id)
+            )?
+        )
+    );
+    //
+    let cur_scene = Rc::new(
+        RefCell::new(
+            EntityInstance::new(&engine, 
+                Rc::clone(
+                    entity_defs.get(&cur_scene_id)
+                    .expect("entity_defs.get(&scene_id) should have had the scene's definition by now")
+                )
+            )?
+        )
+    );
+
+    // Register API closures, which need to 
+    // capture the game-loop's environment
+    let api_cur_scene = Rc::clone(&cur_scene);
+    engine.register_fn("get_cur_scene", move || -> Result<scene::Scene, Box<EvalAltResult>> {
+        //
+        let cur_scene_borrow = api_cur_scene.try_borrow();
+        //
+        if cur_scene_borrow.is_err() {
+            //
+            return Err("Can't use the 'get_cur_scene' function inside a scene script! Use the Scene object instead.".into());
+        }
+        //
+        Ok(cur_scene_borrow.unwrap().scope.get_value::<scene::Scene>("Scene")
+        .expect("The Scene object should have been created in this scene's scope by the time it was created"))
+    });
+
+    // The API is done!
+    Ok((engine, state_manager, cur_scene, cur_scene_id))
 }
 
 //
 pub fn run_game() -> Result<(), (EntityRow, Box<EvalAltResult>)>
 {
     // Create the entity definitions hash map.
-    let mut entity_defs: HashMap<u32,EntityDefinition> = HashMap::new();
+    let mut entity_defs: HashMap<u32,Rc<EntityDefinition>> = HashMap::new();
     // Create the API 'Engine', and the state manager instance.
-    let (api_engine, mut state_manager) = create_api(&mut entity_defs)?;
-
-    // Receive the rowid of the initial scene from the the state manager's config
-    let scene_id = state_manager.definition.config["initial-scene"].as_int()
-        .expect("The value of 'initial-scene' in the state manager's config should be an integer") as u32;
-    //
-    if !entity_defs.contains_key(&scene_id) {
-        //
-        entity_defs.insert(scene_id, EntityDefinition::new(&api_engine, EntityRow::Scene(scene_id))?);
-    }
-    //
-    let mut scene = EntityInstance::new(&api_engine, entity_defs.get(&scene_id)
-        .expect("entity_defs.get(&scene_id) should have had the scene's definition by now"))?;
+    let (api_engine, mut state_manager, 
+        cur_scene, mut cur_scene_id) = create_api(&mut entity_defs)?;
 
     // Call the 'create' function on the state manager instance.
     state_manager.call_fn(&api_engine, "create", ())?;
     //
-    scene.call_fn(&api_engine, "create", ())?;
+    cur_scene.borrow_mut().call_fn(&api_engine, "create", ())?;
 
-    // let mut objects: Vec<EntityInstance> = Vec::new();
+    // ..game loop..
 
     // Done!
     Ok(())
--- instance/game-engine/src/game/scene.rs
+++ instance/game-engine/src/game/scene.rs
@@ -1,16 +1,20 @@
+
 use rhai::{Engine, Map};
 
+//
 #[derive(Clone)]
 pub struct Layer {
     name: String,
 }
 
+//
 impl Layer {
     pub fn get_name(&mut self) -> String { self.name.clone() }
 
-    pub fn to_string(layer: &mut Self) -> String { layer.get_name() }
+    pub fn to_string(&mut self) -> String { self.name.clone() }
 }
 
+//
 #[derive(Clone)]
 pub struct Camera {
     x: f64,
@@ -18,20 +22,28 @@ pub struct Camera {
     zoom: f32,
 }
 
+//
 impl Camera {
     pub fn get_x(&mut self) -> f64 { self.x.clone() }
     pub fn get_y(&mut self) -> f64 { self.y.clone() }
     pub fn get_zoom(&mut self) -> f32 { self.zoom.clone() }
 
     pub fn set_x(&mut self, value: f64) { self.x = value; }
+    pub fn set_x_rhai_int(&mut self, value: rhai::INT) { self.x = value as f64; }
+    pub fn set_x_rhai_float(&mut self, value: rhai::FLOAT) { self.x = value as f64; }
     pub fn set_y(&mut self, value: f64) { self.y = value; }
+    pub fn set_y_rhai_int(&mut self, value: rhai::INT) { self.y = value as f64; }
+    pub fn set_y_rhai_float(&mut self, value: rhai::FLOAT) { self.y = value as f64; }
     pub fn set_zoom(&mut self, value: f32) { self.zoom = value; }
+    pub fn set_zoom_rhai_int(&mut self, value: rhai::INT) { self.zoom = value as f32; }
+    pub fn set_zoom_rhai_float(&mut self, value: rhai::FLOAT) { self.zoom = value as f32; }
 
-    pub fn to_string(camera: &mut Self) -> String { 
-        format!("Camera:\n\tx - {x} y- {y} zoom - {zoom}", x = camera.x, y = camera.y, zoom = camera.zoom)
+    pub fn to_string(&mut self) -> String { 
+        format!("Camera:\n\tx - {x} y - {y} zoom - {zoom}", x = self.x, y = self.y, zoom = self.zoom)
     }
 }
 
+//
 #[derive(Clone)]
 pub struct Scene {
     width: u64,
@@ -42,40 +54,112 @@ pub struct Scene {
     camera: Camera,
 }
 
+//
 impl Scene {
     pub fn get_width(&mut self) -> u64 { self.width.clone() }
     pub fn get_height(&mut self) -> u64 { self.height.clone() }
     pub fn get_inside_color(&mut self) -> String { self.in_color.clone() }
     pub fn get_outside_color(&mut self) -> String { self.out_color.clone() }
     pub fn get_layers(&mut self) -> Vec<Layer> { self.layers.clone() }
-    pub fn get_layer(&mut self, index: usize) -> Layer { self.layers[index].clone() }
+    // pub fn index_layer(&mut self, index: usize) -> Layer { self.layers[index].clone() }
     pub fn get_camera(&mut self) -> Camera { self.camera.clone() }
 
     pub fn set_width(&mut self, value: u64) { self.width = value; }
+    pub fn set_width_rhai_int(&mut self, value: rhai::INT) { self.width = value as u64; }
+    pub fn set_width_rhai_float(&mut self, value: rhai::FLOAT) { self.width = value as u64; }
     pub fn set_height(&mut self, value: u64) { self.height = value; }
+    pub fn set_height_rhai_int(&mut self, value: rhai::INT) { self.height = value as u64; }
+    pub fn set_height_rhai_float(&mut self, value: rhai::FLOAT) { self.height = value as u64; }
     pub fn set_inside_color(&mut self, value: String) { self.in_color = value; }
     pub fn set_outside_color(&mut self, value: String) { self.out_color = value; }
+    pub fn set_camera(&mut self, value: Camera) { self.camera = value; }
 
-    pub fn to_string(scene: &mut Self) -> String {
-        // let layers_str = String::new();
-        format!("Scene:\n\twidth - {width} height - {height}\n\tinside color - {in_color} outside color -{out_color}\n\t{camera}\n", 
-            width = scene.width, height = scene.height, in_color = scene.in_color, out_color = scene.out_color, 
-            camera = Camera::to_string(&mut scene.camera))
+    pub fn to_string(&mut self) -> String {
+        let mut layers_str = String::new();
+        for layer in &self.layers {
+            layers_str.insert_str(layers_str.len(), &format!("\n\t{}", &layer.name))
+        }
+        format!("Scene:\n\twidth - {width} height - {height}\n\tinside color - {in_color} outside color - {out_color}\n\t{camera}\n\tLayers:{layers}", 
+            width = self.width, height = self.height, in_color = self.in_color, out_color = self.out_color, 
+            camera = &mut self.camera.to_string(), layers = layers_str)
     }
 }
 
+//
 pub fn create_scene(engine: &Engine, config: &Map) -> Scene {
+    //
     let mut layers_vec: Vec<Layer> = Vec::new();
+    //
+    for map in config["layers"].clone().into_typed_array::<Map>()
+    .expect("Every scene's config should contain a 'layers' array, which should only have object-like members.") {
+        //
+        layers_vec.push( Layer { name: map["name"].clone().into_string()
+        .expect("Every member in the 'layers' array of a scene's config should have a string 'name' attribute.") } );
+    }
+    //
     Scene {
-        width: config["width"].as_int().expect("The value of 'width' in a scene's config should be an integer") as u64,
-        height: config["height"].as_int().expect("The value of 'height' in a scene's config should be an integer") as u64,
-        in_color: config["background-color"].clone().as_string().expect("The value of 'background-color' in a scene's config should be an string"),
-        out_color: config["outside-color"].clone().as_string().expect("The value of 'outside-color' in a scene's config should be an string"),
-        camera: Camera { 
-                x: config["camera-position.x"].as_int().expect("The x value of 'camera-position' in a scene's config should be an integer") as f64, 
-                y: config["camera-position.y"].as_int().expect("The y value of 'camera-position' in a scene's config should be an integer") as f64, 
-                zoom: 1 as f32 
-            },
+        //
+        width: { 
+            if config["width"].is_int() {
+                config["width"].as_int()
+                .expect("Every scene's config should contain an integer 'width' attribute.") as u64
+            }
+            else if config["width"].is_float() {
+                config["width"].as_float()
+                .expect("Every scene's config should contain an integer 'width' attribute.") as u64
+            } else {
+                config["width"].clone_cast::<u64>()
+            }
+        },
+        //
+        height: { 
+            if config["height"].is_int() { 
+                config["height"].as_int()
+                .expect("Every scene's config should contain an integer 'height' attribute.") as u64
+            }
+            else if config["height"].is_float() {
+                config["height"].as_float()
+                .expect("Every scene's config should contain an integer 'height' attribute.") as u64
+            } else {
+                config["height"].clone_cast::<u64>()
+            }
+        },
+        //
+        in_color: config["background-color"].clone().into_string()
+        .expect("Every scene's config should contain a string 'background-color' attribute."),
+        //
+        out_color: config["outside-color"].clone().into_string()
+        .expect("Every scene's config should contain a string 'outside-color' attribute."),
+        //
+        camera: Camera {
+            //
+            x: {
+                if config["camera-position"].clone_cast::<Map>()["x"].is_float() {
+                    config["camera-position"].clone_cast::<Map>()["x"].as_float()
+                    .expect("Every scene's config should contain a 'camera-position' object with 'x' and 'y' float attributes.") as f64
+                } else if config["camera-position"].clone_cast::<Map>()["x"].is_int() {
+                    config["camera-position"].clone_cast::<Map>()["x"].as_int()
+                    .expect("Every scene's config should contain a 'camera-position' object with 'x' and 'y' float attributes.") as f64
+                } else {
+                    config["camera-position"].clone_cast::<Map>()["x"].clone_cast::<f64>()
+                }
+            }, 
+            //
+            y: {
+                if config["camera-position"].clone_cast::<Map>()["y"].is_float() {
+                    config["camera-position"].clone_cast::<Map>()["y"].as_float()
+                    .expect("Every scene's config should contain a 'camera-position' object with 'x' and 'y' float attributes.") as f64
+                } else if config["camera-position"].clone_cast::<Map>()["y"].is_int() {
+                    config["camera-position"].clone_cast::<Map>()["y"].as_int()
+                    .expect("Every scene's config should contain a 'camera-position' object with 'x' and 'y' float attributes.") as f64
+                } else {
+                    config["camera-position"].clone_cast::<Map>()["y"].clone_cast::<f64>()
+                }
+            }, 
+            //
+            zoom: 1 as f32 
+        },
+        //
         layers: layers_vec,
     }
 }
