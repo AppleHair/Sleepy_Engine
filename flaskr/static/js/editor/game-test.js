f16e0a5cda21119ad0e9534473bf8559afcb5275
AppleHair <eytanzury@gmail.com>
2023-10-12 12:46:56 +0300

    fixed a few browser behavior issues

--- flaskr/static/js/editor/game-test.js
+++ flaskr/static/js/editor/game-test.js
@@ -6,6 +6,11 @@ import initEngine, * as wasmEngine from "./engine-code/game_engine.js";
 let update_handle;
 
 //
+document.addEventListener("contextmenu", (event) => event.preventDefault(), true);
+document.addEventListener("keydown", (event) => event.preventDefault(), true);
+document.addEventListener("keyup", (event) => event.preventDefault(), true);
+
+//
 initEngine().then(() => {
     update_handle = wasmEngine.handle_game();
 });
